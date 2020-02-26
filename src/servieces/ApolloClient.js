9d4e5fe5aa0efc6557b1baeb19f7331f337a30c7
Piotr <pmotak@gmail.com>
2020-02-26 06:58:11 +0100

    amend URl link to working one

--- src/servieces/ApolloClient.js
+++ src/servieces/ApolloClient.js
@@ -3,9 +3,9 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const httpLink = new createHttpLink({
-  uri: 'https://api.graphcms.com/simple/v1/swapi'
+  uri: 'https://swapi.graph.cool/'
 });
-console.log(httpLink)
+
 export const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
